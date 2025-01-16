982c83313b132ad8f1725afa7041ed042881b215
PilarFernandezNieto <apreilmc@gmail.com>
2025-01-16 21:24:22 +0100

    PedidoProducto

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pedido;
+use App\Models\PedidoProducto;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,15 +24,33 @@ class PedidoController extends Controller
     public function store(Request $request)
     {
         // Almacenar un pedido
-
         $pedido = new Pedido;
         $pedido->user_id = Auth::user()->id;
         $pedido->total = $request->total;
         $pedido->save();
 
+        // Obtener el id del pedido
+        $id = $pedido->id;
+
+        // Obtener los productos
+        $productos = $request->productos;
+
+        // Formatear un arreglo
+        $pedido_producto = [];
+        foreach ($productos as $producto) {
+            $pedido_producto[] = [
+                'pedido_id' => $id,
+                'producto_id' => $producto['id'],
+                'cantidad' => $producto['cantidad'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ];
+        }
+        // Almacenar en la bbdd
+        PedidoProducto::insert($pedido_producto);
+
         return [
-            "pedido" => $pedido,
-            'message' => 'Realizando pedido...'
+            'message' => 'Pedido realizado correctamente'
         ];
     }
 
