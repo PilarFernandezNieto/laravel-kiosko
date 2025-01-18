f8a5d30e67aebd53c5e1efd687e0b64e1c2df79a
PilarFernandezNieto <apreilmc@gmail.com>
2025-01-18 13:42:37 +0100

    Envia pedidos con usuarios y productos

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function productos(){
+        return $this->belongsToMany(Producto::class, 'pedido_productos')->withPivot('cantidad');
+    }
 }
