127eff4d5fc8224f9355eadc56bba8f759146c45
PilarFernandezNieto <apreilmc@gmail.com>
2025-01-18 16:59:24 +0100

    Pedido completado

--- routes/api.php
+++ routes/api.php
@@ -9,24 +9,24 @@ use Illuminate\Support\Facades\Route;
 
 
 // Todas las rutas que se coloquen en este middleware, necesitaran un token de autenticación
-Route::middleware('auth:sanctum')->group(function() {
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::post('/logout', [AuthController::class, 'logout'] );
+    Route::post('/logout', [AuthController::class, 'logout']);
 
     // Almacenar pedidos
     Route::apiResource('/pedidos', PedidoController::class);
-
+    //Route::get('/categorias', [CategoriaController::class, 'index']);
+    /** apiResource engloba todos los verbos que engloban las peticiones : GET, POST, PUT, DELETE */
+    Route::apiResource('categorias', CategoriaController::class);
+    Route::apiResource('productos', ProductoController::class);
 });
 
 
-//Route::get('/categorias', [CategoriaController::class, 'index']);
-/** apiResource engloba todos los verbos que engloban las peticiones : GET, POST, PUT, DELETE */
-Route::apiResource('categorias', CategoriaController::class);
-Route::apiResource('productos', ProductoController::class);
+
 
 
 // Autenticación
-Route::post('/registro', [AuthController::class, 'register'] );
-Route::post('/login', [AuthController::class, 'login'] );
+Route::post('/registro', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
