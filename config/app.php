86920781646fe9846311939e4cc98420079444b1
laravel-kiosko-test.nue.dom.my.id <laravel-kiosko-test@nue.domcloud.co>
2025-01-26 13:13:04 +0000

    deploy

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://vue-kiosko.vercel.app'),
 
     /*
     |--------------------------------------------------------------------------
